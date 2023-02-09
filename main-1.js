5da55ba19f2e6f38f211619b86ee7fece0b0f1f9
mayahilbert <35238760+mayahilbert@users.noreply.github.com>
2023-02-09 16:46:35 -0500

    animation test

--- main-1.js
+++ main-1.js
@@ -39,28 +39,50 @@
 let sections = gsap.utils.toArray("section"),
   currentSection = sections[0];
 
-gsap.defaults({ overwrite: "auto", duration: 0.5 });
+gsap.defaults({ overwrite: "auto" });
 
 // stretch out the body height according to however many sections there are.
-gsap.set("body", { height: sections.length * 70 + "%" });
+gsap.set("body", { height: sections.length * 65 + "%" });
 
 // create a ScrollTrigger for each section
 sections.forEach((section, i) => {
   ScrollTrigger.create({
     // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
-    start: () => ((i - 0.5) * innerHeight) / 1.5,
-    end: () => ((i + 0.5) * innerHeight) / 1.5,
+    start: () => ((i - 0.5) * innerHeight) / 2,
+    end: () => ((i + 0.5) * innerHeight) / 2,
     // when a new section activates (from either direction), set the section accordinglyl.
     onToggle: (self) => self.isActive && setSection(section),
+    // scrub: 1,
+    // snap: {
+    //   snapTo: 1,
+    // },
   });
+  if (i > 0) {
+    ScrollTrigger.create({
+      start: () => ((i - 0.5) * innerHeight) / 2,
+      end: () => ((i + 0.5) * innerHeight) / 2,
+      snap: {
+        snapTo: 1,
+        duration: { min: 0, max: 0.1 },
+        delay: 1,
+        snapDirectional: (1, 1),
+        ease: "power1.inOut",
+      },
+    });
+  }
 });
 
 function setSection(newSection) {
   if (newSection !== currentSection) {
-    gsap.to(currentSection, { scale: 2, autoAlpha: 0 });
-    gsap.from(newSection, { scale: 0.5 });
-    gsap.to(newSection, { autoAlpha: 1 });
-    gsap.to(currentSection, { scale: 1 });
+    gsap.to(currentSection, { scale: 2, duration: 1 });
+    gsap.to(currentSection, { autoAlpha: 0, duration: 0.8 });
+    gsap.from(newSection, { scale: 0.6, duration: 0.7 });
+    gsap.to(newSection, { autoAlpha: 1, duration: 0.7 });
+    gsap.to(currentSection, { scale: 1, delay: 1.1 });
+    // gsap.to(currentSection, { scale: 2, autoAlpha: 0 });
+    // gsap.from(newSection, { scale: 0.6 });
+    // gsap.to(newSection, { autoAlpha: 1 });
+    // gsap.to(currentSection, { scale: 1 });
     currentSection = newSection;
   }
 }
