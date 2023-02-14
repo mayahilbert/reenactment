f10439d6b81c5267984b8180dc120776cd3ccf61
mayahilbert <35238760+mayahilbert@users.noreply.github.com>
2023-02-14 10:25:30 -0500

    integrating project display animations

--- main.js
+++ main.js
@@ -27,7 +27,12 @@ function pauseVideo(thisVid) {
   $(thisVideo).removeClass("show");
   vimeoplayer.pause();
 }
-
+var overlayTrigger;
+var overlayContainer = $("#overlay-project");
+let overlaySnaps = [];
+let overlaySections = [],
+  sections = gsap.utils.toArray(".work"),
+  currentSection = sections[0];
 gsap
   .timeline({
     scrollTrigger: {
@@ -59,89 +64,154 @@ $(window).bind("scroll", function () {
     scrollIndicator.style.opacity = "1";
   }
 });
-var overlayTrigger = 0;
+
 var gridCircles = document.querySelectorAll(".gridLink");
-gridCircles.forEach((el) =>
+gridCircles.forEach((el, i) =>
   el.addEventListener("click", (event) => {
     event.preventDefault(); // stop default link navigation
     var target = $(el).data("scroll-target");
-    $("html, body").addClass("overlay-on");
+
+    // $("#overlay-project").css("visibility", "visible");
+    let targetElem = document.querySelector(target),
+      y = targetElem,
+      gridContainer = document.querySelector(".grid"),
+      currentSectionIndex = sections.findIndex(
+        (x) => x.id === $(el).data("scroll-target").replace("#", "")
+      ),
+      sectionCount = 0;
+
+    gsap.defaults({ overwrite: "auto" });
+    // gsap.set(".main-container", { height: "0px" });
     $("html,body").animate(
       {
         scrollTop: 500,
       },
       "500"
     );
-    $("#overlay-project").fadeIn();
-    // $("#overlay-project").css("visibility", "visible");
-    document.location.href = $(el).data("scroll-target");
 
-    // const sections = gsap.utils.toArray(".work");
-    // // sections.reverse();
-    // sections.forEach((section, index) => {
-    //   const work = section.querySelector(".work-inner");
-    //   (notFirst = document.querySelector(".work:not(:first-child) *")),
-    //     (notLast = document.querySelector(".work:not(:last-child) *"));
-    //   if (overlayTrigger < 1) {
-    //     gsap.utils.toArray(".step").forEach((step) => {
-    //       ScrollTrigger.create({
-    //         trigger: step,
-    //         start: "top top",
-    //         end: "center top",
-    //         toggleClass: "active",
-    //       });
-    //     });
+    gsap.set("body", { height: sections.length * 100 + "%" });
 
-    //     // var tl = gsap.timeline({
+    // document.location.href = $(el).data("scroll-target");
 
-    //     //   scrollTrigger: {
-    //     //     trigger: section,
-    //     //     pin: true,
-    //     //     anticipatePin: 1,
-    //     //     scrub: 1,
-    //     //     start: "top top",
-    //     //   },
-    //     // });
-    //     // if (index > 0) {
-    //     // tl.from(work, {
-    //     //   scale: 2,
-    //     //   ease: "ease",
-    //     //   duration: 10,
-    //     // });
-    //     // }
-    //     // if (index < sections.length - 1) {
-    //     //   tl.to(work, {
-    //     //     scale: 2,
-    //     //     ease: "ease",
-    //     //     duration: 15,
-    //     //   });
-    //     // }
+    // create a ScrollTrigger for each section
+    // sections.forEach((section, i) => {
+    var sectionScrollsCreated = new Promise((resolve, reject) => {
+      sections.forEach((section, i) => {
+        // function createSectionScrolls() {
+        //   for (const section of sections) {
+        let overlaySection = ScrollTrigger.create({
+          id: "overlay-section-" + i,
+          // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
+          pinned: true,
+          start: () => (i - 0.5) * innerHeight,
+          end: () => (i + 0.5) * innerHeight,
+          // markers: true,
+          // when a new section activates (from either direction), set the section accordingly.
+          onToggle: (self) => self.isActive && setSection(section),
+        });
+        overlaySections.push(overlaySection);
+        console.log("overlaySections: ");
+        console.log(overlaySections);
+        // if (i > 0) {
+        //   let overlaySnap = ScrollTrigger.create({
+        //     id: "overlay-snap-" + i,
+        //     start: () => (i - 0.5) * innerHeight,
+        //     end: () => (i + 0.5) * innerHeight,
+        //     snap: {
+        //       snapTo: 0.5,
+        //       duration: { min: 0, max: 0.1 },
+        //       delay: 1,
+        //       snapDirectional: (1, 1),
+        //       ease: "power1.inOut",
+        //     },
+        //   });
+        //   overlaySnaps.push(overlaySnap);
+        // }
+        if (i === sections.length - 1) resolve();
+      });
+    });
 
-    //     // console.log("tl " + tl);
-    //   }
-    // });
+    sectionScrollsCreated.then(() => {
+      console.log("All done!");
+      setTimeout(function () {
+        //your code to be executed after 1 second
+        gsap.to(window, {
+          duration: 0,
+          scrollTo: currentSectionIndex * window.innerHeight,
+          overwrite: true,
+        });
+        setSection(sections[currentSectionIndex]);
+
+        console.log(
+          "currentSectionIndex: " +
+            currentSectionIndex +
+            " currentSection: " +
+            currentSection +
+            " scrollTo " +
+            currentSectionIndex * window.innerHeight
+        );
+        $("html, body").addClass("overlay-on");
+
+        $("#overlay-project").fadeIn();
+      }, 500);
+    });
+
+    function setSection(newSection) {
+      if (newSection !== currentSection) {
+        gsap.to(currentSection, { autoAlpha: 0, duration: 0.7 });
+        gsap.to(currentSection, { scale: 1.7, duration: 0.7 });
+        gsap.to(currentSection, { scale: 1, delay: 0.9 });
+        gsap.from(newSection, { scale: 0.6, duration: 0.7 });
+        gsap.to(newSection, { autoAlpha: 1, duration: 0.7 });
+        console.log(
+          "setting section: " + currentSection.id + " to " + newSection.id
+        );
+        currentSection = newSection;
+      }
+    }
+    // handles the infinite part, wrapping around at either end....
+    ScrollTrigger.create({
+      id: "overlay-loop",
+      start: 1,
+      end: () => ScrollTrigger.maxScroll(window) - 1,
+      onLeaveBack: (self) => self.scroll(ScrollTrigger.maxScroll(window) - 2),
+      onLeave: (self) => self.scroll(2),
+    }).scroll(2);
+
     overlayTrigger = 1;
+
     event.stopPropagation();
   })
 );
 
 $(document).on("click", function () {
+  if (overlayTrigger === 1) {
+    overlaySections.forEach((overlaySection) => overlaySection.kill());
+    overlaySnaps.forEach((overlaySnap) => overlaySnap.kill());
+    overlaySections = [];
+    overlaySnaps = [];
+    // overlayLoop.kill();
+  }
   vidContainers.forEach((el) => {
     pauseVideo(el);
   });
 
   $("#overlay-project").fadeOut();
+  gsap.to(currentSection, { autoAlpha: 0 });
+  gsap.to(currentSection, { scale: 0.6 });
   // $("#overlay-project").css("visibility", "hidden");
+  $("body").css("height", "100%");
 
+  $("html, body").removeClass("overlay-on");
   $("html, body").animate(
     {
       scrollTop: $(document).height(),
     },
     "fast"
   );
-  $("html, body").removeClass("overlay-on");
   $("#overlay-project").scrollTop(0);
   document.location.href = "#";
+  overlayTrigger = 0;
 });
 
 $(".work > *").on("click", function (event) {
